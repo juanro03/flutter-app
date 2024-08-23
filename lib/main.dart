af688c205d67444028dfeeb10d6f4132819df41f
juanro03 <roldanjuan2003@gmail.com>
2024-08-23 20:41:47 -0300

    creacion proyecto

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:carpool_app/widgets/video_card.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CarpoolApp"),
+        backgroundColor: Colors.red,
+        actions: [
+          Icon(Icons.search),
+          Icon(Icons.camera),
+          Icon(Icons.person)
+          ],
+      ),
+      body: Column(children: [
+        VideoCard()
+      ],),
+    );
+  }
+}
---
+++ lib/widgets/video_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VideoCard extends StatelessWidget {
+  const VideoCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset("assets/images/img1.jpg")
+      );
+  }
+}
